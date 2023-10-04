9a7a13f5663829a0f424ce9d519711f4e84f7520
Phillip Quiza <pquiza@gmail.com>
2023-10-04 09:34:44 +0100

    better initialization

--- init.sh
+++ init.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
 # initialize pathogen
-mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+#mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+#curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+mkdir ~/.vim/backup
+mkdir ~/.vim/tmp
+
+# link config files
+#ln -s ~/dev/dotfiles/.zshrc ~/.zshrc
+#ln -s ~/dev/dotfiles/.gitconfig ~/.gitconfig
+#ln -s ~/dev/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dev/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dev/dotfiles/vim/colors ~/.vim/colors
+ln -s ~/dev/dotfiles/vim/ftplugin ~/.vim/ftplugin
